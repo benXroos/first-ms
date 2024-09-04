1d77ae4fdd00ffa9e8a6478e490463d0f57fd3de
Benito <benito.ramon@globant.com>
2024-09-03 23:14:37 -0300

    cambios de properties

--- src/main/java/org/example/personService/model/Person.java
+++ src/main/java/org/example/personService/model/Person.java
@@ -1,12 +1,11 @@
 package org.example.personService.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
 @Entity
+@Table(name="person")
 public class Person {
 
     @Id
